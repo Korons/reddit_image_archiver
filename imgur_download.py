85643ee77fc9dcf1585ab27f72fc0e591187dffb
Korons <william.sweeting@vfemail.net>
2016-01-26 03:11:44 -0500

    now it doesn't try to redownload existing files

--- imgur_download.py
+++ imgur_download.py
@@ -3,21 +3,33 @@ import re
 import urllib.request, urllib.parse, urllib.error, urllib
 import os
 home = os.getenv("HOME")
-
-SUBREDDITS= home + '/.config/imgur_down/subreddits.txt'
+downloaded = home + '/.config/imgur_down/downloaded.txt'
+SUBREDDITS = home + '/.config/imgur_down/subreddits.txt'
 with open(SUBREDDITS) as f:
 	lines = f.read().splitlines()
+	print (lines)
 for i in lines:
+	print (i)
 	url = "https://reddit.com/r/" + i + ".json"
 	req = urllib.request.Request(
     url, 
+    # The user-agent we send so we don't get massively limited
     headers={'User-Agent': "abotbyATGUNAT"}
 	)
 	print (url)
 	reddit_call = urllib.request.urlopen(req)
 	result = reddit_call.read().decode('utf-8')
-	results = re.findall('http://imgur.com/a......', result)
-	for u in results:
+	results = re.findall('https?://imgur.com/a......', result)
+	print (results)
+	direct_links = re.findall('http://imgur.com/a/', result)
+	for image_url in results:
+		# This can and should be changed to a os.mkdir
 		os.system("mkdir -p /home/wil/Pictures/Imguralbums/" + i)
 		os.chdir("/home/wil/Pictures/Imguralbums/" + i)
-		os.system("imgurdl " + u)
+		if image_url not in open(downloaded).read():
+			os.system("imgurdl " + image_url)
+			# We write the downloaded url to a file so we can quickly skip already downloaded files
+			f = open(downloaded,'a')
+			f.write(image_url + '\n')
+			f.close() 
+			
