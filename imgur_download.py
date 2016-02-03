31dfa530093a418588f34d80158c74b28790b03e
wil <william.sweeting@vfemail.net>
2016-02-03 17:06:11 -0500

    Added logging

--- imgur_download.py
+++ imgur_download.py
@@ -2,9 +2,11 @@ import sys
 import re
 import urllib.request, urllib.parse, urllib.error, urllib
 import os
+import time
 home = os.getenv("HOME")
 downloaded = home + '/.config/imgur_down/downloaded.txt'
 SUBREDDITS = home + '/.config/imgur_down/subreddits.txt'
+logfile = home + '/Pictures/Imguralbums/log.txt'
 with open(SUBREDDITS) as f:
 	lines = f.read().splitlines()
 	print (lines)
@@ -19,7 +21,8 @@ for i in lines:
 	reddit_call = urllib.request.urlopen(req)
 	result = reddit_call.read().decode('utf-8')
 	results = re.findall('https?://imgur.com/a......', result)
-	direct_links = re.findall('http://imgur.com/a/', result)
+	direct_links = re.findall('https?:\/\/.*.png', result)
+	print (direct_links)
 	for image_url in results:
 		# This can and should be changed to a os.mkdir
 		os.system("mkdir -p /home/wil/Pictures/Imguralbums/" + i)
@@ -31,6 +34,13 @@ for i in lines:
 			f.write(image_url + '\n')
 			f.close() 
 for i in lines:
-	os.chdir("/home/wil/Pictures/Imguralbums/" + i)
+	os.chdir(home + "/Pictures/Imguralbums/" + i)
+	# These system calls seem to hang the program some times no idea why
 	os.system('for i in */; do zip -r "${i%/}.cbr" "$i" -x *.cbr; done')
 	os.system('rm -r */')
+	# Logging 
+	time = time.strftime("%H:%M:%S:%d/%m/%Y")
+	f = open(logfile,'r+')
+	f.write(("Ran at " + time))
+	f.close
+
