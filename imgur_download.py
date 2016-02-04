f1e6cbb108d8f8e08c8759ea8ed2f918d8576afb
wil <william.sweeting@vfemail.net>
2016-02-04 14:05:51 -0500

    removed hardcored dir

--- imgur_download.py
+++ imgur_download.py
@@ -22,26 +22,31 @@ for i in lines:
 	result = reddit_call.read().decode('utf-8')
 	results = re.findall('https?://imgur.com/a......', result)
 	# This regex is from https://stackoverflow.com/questions/169625/regex-to-check-if-valid-url-that-ends-in-jpg-png-or-gif
-	direct_links_png = re.findall('^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png)$', result)
+	direct_links = re.findall('^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png)$', result)
 	print (direct_links)
 	for image_url in results:
 		# This can and should be changed to a os.mkdir
 		os.system("mkdir -p /home/wil/Pictures/Imguralbums/" + i)
-		os.chdir("/home/wil/Pictures/Imguralbums/" + i)
+		os.chdir(home + "/Pictures/Imguralbums/" + i)
 		if image_url not in open(downloaded).read():
 			os.system("imgurdl " + image_url)
 			# We write the downloaded url to a file so we can quickly skip already downloaded files
 			f = open(downloaded,'a')
 			f.write(image_url + '\n')
 			f.close() 
+	for d_image_url in direct_links:
+		char_set = string.ascii_uppercase + string.digits
+		randstring = ''.join(random.sample(char_set*6, 6))
+		urllib.urlretrieve(d_image_url,)
+
 for i in lines:
 	os.chdir(home + "/Pictures/Imguralbums/" + i)
 	# These system calls seem to hang the program some times no idea why
 	os.system('for i in */; do zip -r "${i%/}.cbr" "$i" -x *.cbr; done')
-	#os.system('rm -r */')
+	# os.system('rm -r */')
 	# Logging 
-	time = time.strftime("%H:%M:%S:%d/%m/%Y")
-	f = open(logfile,'r+')
-	f.write(("Ran at " + time))
-	f.close
+	# time = time.strftime("%H:%M:%S:%d/%m/%Y")
+	# f = open(logfile,'r+')
+	# f.write(("Ran at " + time))
+	# f.close
 
